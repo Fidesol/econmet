65a218301a9288a6da7445cbeffc4ef3d57e8a69
root <root@usuario-M61PME-S2P.(none)>
2013-07-03 08:40:43 +0200

    test

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(name='YourAppName', version='1.0',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['greenlet', 'gevent',
+      install_requires=['greenlet', 'gevent','Django>=1.3', 'django-rosetta==0.6.6',
+                        'django-tinymce==1.5.1b2', 'ccsm==0.9.5.92', 'South==0.7.4',
                         #  'MySQL-python',
                         #  'pymongo',
                         #  'psycopg2',
